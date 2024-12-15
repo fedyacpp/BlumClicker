31d8b6f4d33578342a2feb6aa646e94e54091e02
fedyacpp <fedyacpp@gmail.com>
2024-12-15 09:44:14 +0300

    hope I didn't break anything

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ class TelegramBot:
     FRAME_SKIP = 1
     TARGET_ID = 1
     SETTINGS_FILE = "settings.json"
+    PLAY_KEYWORDS = ["Play", "Играть"]
 
     def __init__(self):
         self.stop_signal = False
@@ -60,12 +61,14 @@ class TelegramBot:
 
         self.load_settings()
         self.model = self.load_model()
+        
+        self.reader = easyocr.Reader(['en'], gpu=torch.cuda.is_available())
 
         self.window = self.find_telegram_window()
         if not self.window:
             self.console.log("[red]Telegram window not found. Exiting...[/red]")
             exit()
-
+        
         self.keyboard_thread = threading.Thread(target=self.check_keyboard, daemon=True)
         self.keyboard_thread.start()
 
@@ -115,7 +118,6 @@ class TelegramBot:
                 if self.pause_signal or self.settings_signal:
                     time.sleep(0.1)
                     continue
-
                 window_rect = win32gui.GetWindowRect(hwnd)
                 bbox = {
                     "left": window_rect[0],
@@ -123,7 +125,6 @@ class TelegramBot:
                     "width": window_rect[2] - window_rect[0],
                     "height": window_rect[3] - window_rect[1],
                 }
-
                 screenshot = sct.grab(bbox)
                 img = np.array(screenshot)
                 img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
@@ -132,10 +133,8 @@ class TelegramBot:
     def perform_click(self, x, y):
         win32api.SetCursorPos((x, y))
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN | win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
-
         click_key = (x, y)
         self.click_counters[click_key] = self.click_counters.get(click_key, 0) + 1
-
         return x, y, self.click_counters[click_key]
 
     def check_keyboard(self):
@@ -234,7 +233,6 @@ class TelegramBot:
             json.dump(settings, file, indent=4)
 
     def show_settings_panel(self):
-
         class SettingsWindow(QtWidgets.QDialog):
             def __init__(self, bot_instance, parent=None):
                 super().__init__(parent)
@@ -332,8 +330,9 @@ class TelegramBot:
         if self.show_debug_window and self.debug_image is not None:
             cv2.imshow("Debug Window", self.debug_image)
             hwnd = win32gui.FindWindow(None, "Debug Window")
-            win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 0, 0, 0, 0,
-                                  win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
+            if hwnd:
+                win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 0, 0, 0, 0,
+                                      win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
             cv2.waitKey(1)
         else:
             if cv2.getWindowProperty("Debug Window", cv2.WND_PROP_VISIBLE) >= 1:
@@ -352,19 +351,63 @@ class TelegramBot:
         return model
 
     def detect_play_button(self, screenshot, bbox, results_queue):
-        reader = easyocr.Reader(['en'], gpu=torch.cuda.is_available())
-        result = reader.readtext(screenshot)
-
+        result = self.reader.readtext(screenshot)
         for (bbox_coords, text, prob) in result:
-            if "Play" in text:
-                (top_left, top_right, bottom_right, bottom_left) = bbox_coords
-                top_left = (int(top_left[0]), int(top_left[1]))
-                bottom_right = (int(bottom_right[0]), int(bottom_right[1]))
-                x, y, w, h = top_left[0], top_left[1], bottom_right[0] - top_left[0], bottom_right[1] - top_left[1]
-                self.console.log(f"'Play' detected at ({x}, {y}, {w}, {h})", style="green")
-                results_queue.put((x + w // 2, y + h // 2))
-                return
+            for keyword in self.PLAY_KEYWORDS:
+                if keyword in text:
+                    (top_left, top_right, bottom_right, bottom_left) = bbox_coords
+                    top_left = (int(top_left[0]), int(top_left[1]))
+                    bottom_right = (int(bottom_right[0]), int(bottom_right[1]))
+                    x, y = top_left[0], top_left[1]
+                    w, h = bottom_right[0] - top_left[0], bottom_right[1] - top_left[1]
+                    self.console.log(f"'{keyword}' detected at ({x}, {y}, {w}, {h})", style="green")
+                    results_queue.put((x + w // 2, y + h // 2))
+                    return
         results_queue.put(None)
+
+    def handle_predictions(self, prediction, bbox, image):
+        if not prediction:
+            return
+        det = prediction[0]
+        if det.boxes is None:
+            return
+
+        boxes = det.boxes.xyxy.cpu().numpy()
+        scores = det.boxes.conf.cpu().numpy()
+        class_ids = det.boxes.cls.cpu().numpy().astype(int)
+
+        self.debug_image = image.copy()
+        sorted_indices = np.argsort(boxes[:, 1])
+
+        for i in sorted_indices:
+            box = boxes[i]
+            score = scores[i]
+            class_id = class_ids[i]
+
+            if score > self.CONFIDENCE_THRESHOLD:
+                x1, y1, x2, y2 = map(int, box)
+                width_scale = bbox["width"] / 640
+                height_scale = bbox["height"] / 640
+                x1 = int(x1 * width_scale)
+                y1 = int(y1 * height_scale)
+                x2 = int(x2 * width_scale)
+                y2 = int(y2 * height_scale)
+
+                color = (0, 255, 0) if class_id == self.TARGET_ID else (255, 0, 0)
+                cv2.rectangle(self.debug_image, (x1, y1), (x2, y2), color, 2)
+                cv2.putText(self.debug_image, f'{class_id}: {score:.2f}', (x1, y1 - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
+
+                if class_id == self.TARGET_ID or (self.click_all_bombs and class_id == 0):
+                    x1 += bbox["left"]
+                    y1 += bbox["top"]
+                    x2 += bbox["left"]
+                    y2 += bbox["top"]
+                    time.sleep(self.delay_before_click)
+                    self.last_click_info = self.perform_click((x1 + x2) // 2, (y1 + y2) // 2)
+                    time.sleep(self.delay_between_clicks)
+
+        self.update_debug_window()
 
     def run(self):
         self.bring_window_to_foreground()
@@ -389,13 +432,12 @@ class TelegramBot:
 
                 if frame_count % self.FRAME_SKIP == 0:
                     preprocessed_frame = self.preprocess_image(image)
-
                     with self.model_lock:
                         with torch.no_grad():
                             prediction = self.model(preprocessed_frame)
                 else:
                     prediction = []
-
+                    
                 if self.auto_play and (ocr_thread is None or not ocr_thread.is_alive()):
                     ocr_thread = threading.Thread(target=self.detect_play_button, args=(image, bbox, results_queue))
                     ocr_thread.start()
@@ -409,46 +451,7 @@ class TelegramBot:
                 except queue.Empty:
                     pass
 
-                if prediction:
-                    det = prediction[0]
-                    if det.boxes is not None:
-                        boxes = det.boxes.xyxy.cpu().numpy()
-                        scores = det.boxes.conf.cpu().numpy()
-                        class_ids = det.boxes.cls.cpu().numpy().astype(int)
-
-                        self.debug_image = image.copy()
-
-                        sorted_indices = np.argsort(boxes[:, 1])
-
-                        for i in sorted_indices:
-                            box = boxes[i]
-                            score = scores[i]
-                            class_id = class_ids[i]
-
-                            if score > self.CONFIDENCE_THRESHOLD:
-                                x1, y1, x2, y2 = map(int, box)
-                                width_scale = bbox["width"] / 640
-                                height_scale = bbox["height"] / 640
-                                x1 = int(x1 * width_scale)
-                                y1 = int(y1 * height_scale)
-                                x2 = int(x2 * width_scale)
-                                y2 = int(y2 * height_scale)
-
-                                color = (0, 255, 0) if class_id == self.TARGET_ID else (255, 0, 0)
-                                cv2.rectangle(self.debug_image, (x1, y1), (x2, y2), color, 2)
-                                cv2.putText(self.debug_image, f'{class_id}: {score:.2f}', (x1, y1 - 10),
-                                            cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
-
-                                if class_id == self.TARGET_ID or (self.click_all_bombs and class_id == 0):
-                                    x1 += bbox["left"]
-                                    y1 += bbox["top"]
-                                    x2 += bbox["left"]
-                                    y2 += bbox["top"]
-                                    time.sleep(self.delay_before_click)
-                                    self.last_click_info = self.perform_click((x1 + x2) // 2, (y1 + y2) // 2)
-                                    time.sleep(self.delay_between_clicks)
-
-                        self.update_debug_window()
+                self.handle_predictions(prediction, bbox, image)
 
                 current_time = time.time()
                 elapsed_time = current_time - last_frame_time
@@ -456,41 +459,4 @@ class TelegramBot:
                 last_frame_time = current_time
 
                 target_frame_time = 1 / self.fps_lock
-                if elapsed_time < target_frame_time:
-                    time.sleep(target_frame_time - elapsed_time)
-
-                system_info = self.get_system_info()
-                if self.last_click_info:
-                    x, y, count = self.last_click_info
-                    click_info_panel = Panel(f"Last click: ({x}, {y})\nClicks: {count}", title="Click Info", border_style="yellow")
-                else:
-                    click_info_panel = Panel("No clicks", title="Click Info", border_style="yellow")
-
-                system_info_panel = Panel(
-                    f"FPS: {fps:.2f}\nCPU: {system_info['CPU Usage']}\nRAM: {system_info['Memory Usage']}\nRAM Total: {system_info['Total Memory']}\nGPU: {system_info['GPU']}\nGPU Usage: {system_info['GPU Usage']}",
-                    title="System Info",
-                    border_style="green",
-                )
-
-                hotkeys_panel = Panel("CTRL+Q - Exit\nCTRL+X - Pause/Resume\nCTRL+W - Settings", title="Hotkeys",
-                                      border_style="magenta")
-
-                layout = Layout()
-                layout.split_row(Layout(name="left"), Layout(self.messages_panel, name="right"))
-                layout["left"].split_column(Layout(click_info_panel), Layout(system_info_panel), Layout(hotkeys_panel))
-
-                live.update(layout)
-                frame_count += 1
-
-        if cv2.getWindowProperty("Debug Window", cv2.WND_PROP_VISIBLE) >= 1:
-            cv2.destroyAllWindows()
-
-    def __del__(self):
-        self.stop_signal = True
-        if self.keyboard_thread.is_alive():
-            self.keyboard_thread.join()
-
-
-if __name__ == "__main__":
-    bot = TelegramBot()
-    bot.run()
+           
